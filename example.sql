3f6bab895bf39e2740ff37653a9058e23e700ffa
kkamikoon <rootsik1221@gmail.com>
2020-12-18 01:13:20 +0900

    edit syntax error on example.sql

--- example.sql
+++ example.sql
@@ -4,10 +4,10 @@ use wol_example;
 INSERT INTO users(  uid, email, password)       VALUES("kkamikoon",     "kkamikoon@gmail.com", "0bdf0332166a949291a3dd410046be5269dbcff5b7b9edbd897df6d9ac5e4ec855e491c6fd150ad9bf4af1d353e0624f3427ec843e0c4a2319349396efb23989");
 
 -- configs  dummy
-INSERT INTO configs(key, value)                 VALUES("wol_domain",    "wol.example.kkamikoon.com");
-INSERT INTO configs(key, value)                 VALUES("domain_check",  1);
-INSERT INTO configs(key, value)                 VALUES("title_tag",     "KKAMIKOON - WOL -Example");
-INSERT INTO configs(key, value)                 VALUES("main_title",    "KKAMIKOON - WOL -Example");
+INSERT INTO configs(`key`, `value`)             VALUES("wol_domain",    "wol.example.kkamikoon.com");
+INSERT INTO configs(`key`, `value`)             VALUES("domain_check",  1);
+INSERT INTO configs(`key`, `value`)             VALUES("title_tag",     "KKAMIKOON - WOL -Example");
+INSERT INTO configs(`key`, `value`)             VALUES("main_title",    "KKAMIKOON - WOL -Example");
 
 -- hosts    dummy
 INSERT INTO hosts(  name, mac, ip, broadcast)   VALUES("[ALL]",         "FF:FF:FF:FF:FF:FF", "255.255.255.255", 1);
